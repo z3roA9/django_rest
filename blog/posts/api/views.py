56c8cb92e677c4b4ed3c4e94bd880bde227ed764
z3roA9 <z3roA9@gmail.com>
2019-08-24 19:05:05 +0700

    final crud api

--- blog/posts/api/views.py
+++ blog/posts/api/views.py
@@ -10,7 +10,7 @@ class PostListView(generics.ListAPIView):
 class PostCreateView(generics.CreateAPIView):
     queryset = Post.objects.all()
     serializer_class = serializers.PostSerializers
-    
+
     def create(self, request, *args, **kwargs):
         super(PostCreateView, self).create(request, args, kwargs)
         response = {
@@ -18,4 +18,34 @@ class PostCreateView(generics.CreateAPIView):
             "message": "Success",
             "result": request.data
         }
+        return Response(response)
+
+class PostDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Post.objects.all()
+    serializer_class = serializers.PostSerializers
+
+    def retrieve(self, request, *args, **kwargs):
+        super(PostDetailView, self).retrieve(request, args, kwargs)
+        instance = self.get_object()
+        serializer = self.get_serializer(instance)
+        data = serializer.data
+        response = {"status_code": status.HTTP_200_OK,
+                    "message": "Successfully retrieved",
+                    "result": data}
+        return Response(response)
+
+    def update(self, request, *args, **kwargs):
+        super(PostDetailView, self).update(request, args, kwargs)
+        instance = self.get_object()
+        serializer = self.get_serializer(instance)
+        data = serializer.data
+        response = {"status_code": status.HTTP_200_OK,
+                    "message": "Successfully updated",
+                    "result": data}
+        return Response(response)
+
+    def delete(self, request, *args, **kwargs):
+        super(PostDetailView, self).delete(request, args, kwargs)
+        response = {"status_code": status.HTTP_200_OK,
+                    "message": "Successfully deleted"}
         return Response(response)
