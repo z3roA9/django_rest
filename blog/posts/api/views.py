0fd52b095aa2557f80870660fbb7bdd74678019f
z3roA9 <z3roA9@gmail.com>
2019-08-24 18:32:05 +0700

    add blog post api

--- blog/posts/api/views.py
+++ blog/posts/api/views.py
@@ -6,3 +6,16 @@ from rest_framework.response import Response
 class PostListView(generics.ListAPIView):
     queryset = Post.objects.all()
     serializer_class = serializers.PostSerializers
+
+class PostCreateView(generics.CreateAPIView):
+    queryset = Post.objects.all()
+    serializer_class = serializers.PostSerializers
+    
+    def create(self, request, *args, **kwargs):
+        super(PostCreateView, self).create(request, args, kwargs)
+        response = {
+            "status_code": status.HTTP_200_OK,
+            "message": "Success",
+            "result": request.data
+        }
+        return Response(response)
--- blog/posts/urls.py
+++ blog/posts/urls.py
@@ -3,5 +3,6 @@ from . import views
 from .api import views
 
 urlpatterns = [
-    path('', views.PostListView.as_view(), name=None)
+    path('', views.PostListView.as_view(), name=None),
+    path('create/', views.PostCreateView.as_view(), name=None),
 ]
